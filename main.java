55c08f53c8fe3e3c2d5356602d85b22a144cbcc4
Peashooter243 <alejandrobanda5604@gmail.com>
2024-02-12 22:23:23 -0500

    Tired

--- main.java
+++ main.java
@@ -1,14 +1,14 @@
 // Keywords in Java
 
-public class Main {
+public class main {
   
   public static void main(String[] args) {
-    String class = "COSC237";
+    String className = "COSC237";
     String meeting_time = "Tues/Thurs 9:30-10:45am";
-    String final = "Thursday December 14th, 8:00-10:00am";
+    String finalExam = "Thursday December 14th, 8:00-10:00am";
     
-    System.out.println("CLASS: " + class);
+    System.out.println("CLASS: " + className);
     System.out.println("MEETING TIME: " + meeting_time);
-    System.out.println("FINAL EXAM DATE: " + final);
+    System.out.println("FINAL EXAM DATE: " + finalExam);
   }
 }
